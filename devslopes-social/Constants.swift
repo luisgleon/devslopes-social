467a2a86e59ee2c4f3b574fdf1d013480cb6c4f6
Luis Leon <luisgleon@gmail.com>
2017-07-02 13:07:47 -0400

    Sign in ui custom classes completed

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Luis Guillermo on 2/7/17.
+//  Copyright © 2017 Luis Guillermo. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
