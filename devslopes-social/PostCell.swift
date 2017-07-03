c10303b37cc2744fa5c59de2f24a8edd598f6ba1
Luis Leon <luisgleon@gmail.com>
2017-07-03 11:26:42 -0400

    feed ui part 2 completed

---
+++ devslopes-social/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  devslopes-social
+//
+//  Created by Luis Guillermo on 3/7/17.
+//  Copyright © 2017 Luis Guillermo. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
